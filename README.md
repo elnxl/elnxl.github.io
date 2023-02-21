d9024b74821550463c9321c188faaf899cd7c57b
Nikita Lychanyi <nlychanyi@gmail.com>
2023-02-22 00:19:36 +0300

    static profile new

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ You can visit my [profile](https://elnxl.github.io/)
 ## Local run
 
 ```bash
-export FLASK_APP=app.py; flask run
+python3 app.py
 ```
--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-from app import app
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return redirect(url_for('profile'))
+
+@app.route('/profile', methods=['GET'])
+def profile():
+    return render_template('profile.html')
+
+@app.route("/static/<path:file_path>")
+def files(file_path):
+    return url_for('static', filename=file_path)
+
+if __name__ == "__main__":
+    app.run(debug=False)
